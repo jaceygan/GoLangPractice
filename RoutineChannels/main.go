16320f6950ee42a65f147dc103b5a17ca5e20d3d
jaceygan <jaceygan@gmail.com>
2023-09-30 07:43:55 +0800

    completed RoutineChannels

--- RoutineChannels/main.go
+++ RoutineChannels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -12,7 +13,10 @@ func main() {
 		"http://facebook.com",
 		"http://porkbun.com",
 		"http://storygraph.com",
+		"http://asasasfew.pop",
 	}
+
+	c := make(chan string)
 
 	fmt.Println("Checking the following urls:")
 
@@ -21,17 +25,33 @@ func main() {
 	}
 	fmt.Println("Here we go.....")
 	fmt.Println()
+
+	//initial run
 	for _, u := range urls {
-		printStatus(u)
+		go printStatus(u, c)
 	}
+
+	//subsequent runs based on response from channel
+
+	for l := range c { //keep listening for response from channel c
+		go func(link string) { //function literal (lambda for python)
+			time.Sleep(time.Second * 5)
+			printStatus(link, c)
+		}(l)
+	}
+
 }
 
-func printStatus(u string) {
+func printStatus(u string, c chan string) {
 	_, err := http.Get(u)
 
 	if err != nil {
-		fmt.Println(u, "is down.. :(")
-	}
-	fmt.Println(u, "is up! :)")
+		fmt.Println(u, "is down down down down!!!")
+		fmt.Println(err)
 
+	} else {
+		fmt.Println(u, "is up and running")
+
+	}
+	c <- u
 }
