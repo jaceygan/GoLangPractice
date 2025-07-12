cef0f4587b0be726660d156b8adf3e32aad2cc5d
jaceygan <jaceygan@gmail.com>
2025-07-12 09:24:54 +0800

    update shapes

--- shapes/main.go
+++ shapes/main.go
@@ -10,12 +10,12 @@ type square struct {
 	sideLength float64
 }
 
-type triangle struct {
-	height, base float64
-}
-
 func (s square) getArea() float64 {
 	return s.sideLength * s.sideLength
+}
+
+type triangle struct {
+	height, base float64
 }
 
 func (t triangle) getArea() float64 {
